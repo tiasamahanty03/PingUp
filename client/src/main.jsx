0fb90c1d7947c63bed130a5527f8e2978faba4a5
Tiasa Mahanty <tiasamahanty03@gmail.com>
2026-01-11 16:51:57 +0530

    Pages Done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,23 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
+import { ClerkProvider } from '@clerk/clerk-react'
+import { Provider } from 'react-redux'
+import { store } from './app/store.js'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <BrowserRouter>
+     <Provider store={store}>
+        <App />
+     </Provider>  
+    </BrowserRouter>    
+  </ClerkProvider>
 )
